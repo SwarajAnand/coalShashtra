45848e11fd81a4cdfb132a7b406f421aadc41ffb
Swaraj Anand <swrjnnd@gmail.com>
2023-06-28 19:28:19 +0530

    init

--- src/Components/Cards/DataCards.js
+++ src/Components/Cards/DataCards.js
@@ -11,34 +11,36 @@ function DataCards() {
       <div className="skill-main">
         {data.map((item) => {
           return (
-            <div>
-              <div className="skillDiv">
-                <div>
-                  <h4>
-                    {item.id}
-                    {item.name}
-                  </h4>
-                </div>
-                <div className="cardMain">
-                  <div className="cardMainData">{item.country} </div>
-                  <div className="cardMainData">{item.weight} </div>
-                </div>
-                <div className="cardMain">
+            <a className="dataCardBox" href="/">
+              <div>
+                <div className="skillDiv">
                   <div>
-                    <div className="cardMainData">{item.buy} </div>
-                    <div className="cardMainData">{item.buyAmt} </div>
+                    <h4>
+                      {item.id}
+                      {item.name}
+                    </h4>
                   </div>
-                  <div>
-                    <div className="cardMainData">{item.sell} </div>
-                    <div className="cardMainData">{item.sellAmt} </div>
+                  <div className="cardMain">
+                    <div className="cardMainData">{item.country} </div>
+                    <div className="cardMainData">{item.weight} </div>
                   </div>
-                  <div>
-                    <div className="cardMainData">{item.last} </div>
-                    <div className="cardMainData">{item.lastAmt} </div>
+                  <div className="cardMain">
+                    <div>
+                      <div className="cardMainData">{item.buy} </div>
+                      <div className="cardMainData">{item.buyAmt} </div>
+                    </div>
+                    <div>
+                      <div className="cardMainData">{item.sell} </div>
+                      <div className="cardMainData">{item.sellAmt} </div>
+                    </div>
+                    <div>
+                      <div className="cardMainData">{item.last} </div>
+                      <div className="cardMainData">{item.lastAmt} </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </a>
           );
         })}
       </div>
